375be72d2ce9d1d676cf15fbba1c64890658e78d
CrisisTecno <tecn0crisis0@gmail.com>
2025-03-06 06:10:59 -0400

    [APP-1]: Fonts defined, Banner and Navbar Finish

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,22 +5,7 @@ module.exports = {
       "./public/**/*.html",
     ],
     theme: {
-      extend: {
-        colors: {
-          // Tu paleta DRINKS, por ejemplo
-          drinks: {
-            backgroundPrimaryColor: "#0d0d1a",
-            neonTwoColor:"#e7c500",
-            neonOneColor:"#00ff00",
-            neonThreeColor:"#f82d97",
-            neonFourColor:"#c501e2",
-            neonPrimaryColor:"#00ffff",
-            whiteColor: "#ffffff",
-            darkBlack: "#010001",
-            deepCyan: "#001111",
-          },
-        },
-      },
+      extend: {},
     },
     plugins: [],
   };
